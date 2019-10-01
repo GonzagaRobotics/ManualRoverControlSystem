1b379f5c11c7f1bdfe488ba28b3cdf2f7de80496
Scott K Logan <logans@cottsay.net>
2019-10-01 05:03:38 -0700

    Get some basic tests running (#10)
    * Add some basic testing
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>
    
    * Fix one last flake8 violation
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>
    
    * Call cppcheck explicitly to specify LANGUAGE
    
    * Make all of the tests work with ROS 2
    
    Two tests are disabled while we decided how to handle the difference in
    behavior.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>
    
    * Add a missing test dependency
    
    * Update tests to reflect new ROS 2 turbo behavior
    
    * Add another test dependency
    
    * Stop running the default cppcheck
    
    * Make the test class names unique
    
    * Try re-arranging the ament_cppcheck call
    
    * Give up on getting cppcheck running with special args
    
    * Remove unnecessary shebang.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Change test_joy_twist.py to not have executable bits.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * More test fixes.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ test/differential_joy_launch_test.py
@@ -0,0 +1,39 @@
+import launch
+import launch_ros
+import launch_ros.actions
+
+import pytest
+
+import test_joy_twist
+
+
+@pytest.mark.rostest
+def generate_test_description(ready_fn):
+    teleop_node = launch_ros.actions.Node(
+        package='teleop_twist_joy',
+        node_executable='teleop_node',
+        parameters=[{
+            'axis_linear.x': 1,
+            'axis_angular.yaw': 0,
+            'scale_linear.x': 2.0,
+            'scale_angular.yaw': 3.0,
+            'enable_button': 0,
+        }],
+    )
+
+    return (
+        launch.LaunchDescription([
+            teleop_node,
+            launch.actions.OpaqueFunction(function=lambda context: ready_fn()),
+        ]), locals()
+    )
+
+
+class DifferentialJoy(test_joy_twist.TestJoyTwist):
+
+    def setUp(self):
+        super().setUp()
+        self.joy_msg['axes'] = [0.3, 0.4]
+        self.joy_msg['buttons'] = [1]
+        self.expect_cmd_vel['linear']['x'] = 0.8
+        self.expect_cmd_vel['angular']['z'] = 0.9
---
+++ test/turbo_enable_joy_launch_test.py
@@ -0,0 +1,42 @@
+import launch
+import launch_ros
+import launch_ros.actions
+
+import pytest
+
+import test_joy_twist
+
+
+@pytest.mark.rostest
+def generate_test_description(ready_fn):
+    teleop_node = launch_ros.actions.Node(
+        package='teleop_twist_joy',
+        node_executable='teleop_node',
+        parameters=[{
+            'axis_linear.x': 1,
+            'axis_angular.yaw': 0,
+            'scale_linear.x': 1.0,
+            'scale_linear_turbo.x': 2.0,
+            'scale_angular.yaw': 1.0,
+            'scale_angular_turbo.yaw': 3.0,
+            'enable_button': 0,
+            'enable_turbo_button': 1,
+        }],
+    )
+
+    return (
+        launch.LaunchDescription([
+            teleop_node,
+            launch.actions.OpaqueFunction(function=lambda context: ready_fn()),
+        ]), locals()
+    )
+
+
+class TurboEnableJoy(test_joy_twist.TestJoyTwist):
+
+    def setUp(self):
+        super().setUp()
+        self.joy_msg['axes'] = [0.3, 0.4]
+        self.joy_msg['buttons'] = [1, 1]
+        self.expect_cmd_vel['linear']['x'] = 0.8
+        self.expect_cmd_vel['angular']['z'] = 0.9
