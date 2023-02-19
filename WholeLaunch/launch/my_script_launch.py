2ba42adeed679b9e57ac29dba3fae73a25fbb9f1
James <jamesmsuchor@gmail.com>
2023-02-19 20:50:13 +0000

    add autolaunch stuff

---
+++ WholeLaunch/launch/my_script_launch.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    telop_twist_joy = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('teleop_twist_joy'), 'launch'),
+            '/teleop-launch.py']),launch_arguments={'joy_config': 'xbox','joy_dev':'dev/inputs/js1'}.items(),
+      )
+    return LaunchDescription([
+        telop_twist_joy,
+
+        Node(
+            name='XboxBroker',
+            package='command_broker',
+            executable='XboxBroker',
+            output='screen'
+        )
+    ])
+
+ld = generate_launch_description()
