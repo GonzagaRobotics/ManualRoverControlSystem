0250cb5f6649a393b5eb7b541cb707632b295fb1
Jason <jlunder@zagmail.gonzaga.edu>
2023-04-01 13:57:48 -0700

    update parameters stuff

--- jetson_comm/jetson_comm/CommandReceiver.py
+++ jetson_comm/jetson_comm/CommandReceiver.py
@@ -3,6 +3,8 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSPresetProfiles
+from rcl_interfaces.msg import SetParametersResult
+from rclpy.parameter import Parameter
 from std_msgs.msg import Float32
 import logging
 
@@ -18,42 +20,26 @@ class CommandReciever(Node):
     '''
     def __init__(self):
         super().__init__("CommandReciever")
-        self.manual_control = True
         self.get_logger().set_level(logging.INFO)
         self.get_logger().info('I have initialized up successfully.')
 
-        self.callback_dict = {
-                'motor_command_exposed/left_trigger': self.lt_callback,
-                'motor_command_exposed/right_trigger': self.rt_callback,
-                'motor_command_exposed/left_shoulder': self.lb_callback,
-                'motor_command_exposed/right_shoulder': self.rb_callback,
-                'motor_command_exposed/dpad_lr': self.dpad_lr_callback,
-                'motor_command_exposed/pause': self.pause_callback
-                }
+        self.declare_parameter('manual_control', True)
+        self.manual_control = self.get_parameter('manual_control').value
+        self.add_on_set_parameters_callback(self.parameters_callback)
 
-        topic = 'motor_command_exposed/left_trigger'
-        self.lt_topic = topic
-        self.lt_subscriber =self.get_xbox_subscriber(topic)
-        
-        topic = 'motor_command_exposed/right_trigger'
-        self.rt_topic = topic
-        self.rt_subscriber = self.get_xbox_subscriber(topic)
-        
-        topic = 'motor_command_exposed/left_shoulder'
-        self.lb_topic = topic
-        self.lb_subscriber = self.get_xbox_subscriber(topic)
-        
-        topic = 'motor_command_exposed/right_shoulder'
-        self.rb_topic = topic
-        self.rb_subscriber = self.get_xbox_subscriber(topic)
-        
-        topic = 'motor_command_exposed/dpad_lr'
-        self.dpad_lr_topic = topic
-        self.dpad_lr_subscriber = self.get_xbox_subscriber(topic) 
-    
-        topic = 'motor_command_exposed/pause'
-        self.pause_topic = topic
-        self.pause_subscriber = self.get_xbox_subscriber(topic)
+        self.lt_topic = 'motor_command_exposed/left_trigger'
+        self.rt_topic = 'motor_command_exposed/right_trigger'
+        self.lb_topic = 'motor_command_exposed/left_shoulder'
+        self.rb_topic = 'motor_command_exposed/right_shoulder'
+        self.dpad_lr_topic = 'motor_command_exposed/dpad_lr'
+        self.pause_topic = 'motor_command_exposed/pause'
+
+        self.lt_subscriber =self.get_xbox_subscriber(self.lt_topic)
+        self.rt_subscriber = self.get_xbox_subscriber(self.rt_topic)
+        self.lb_subscriber = self.get_xbox_subscriber(self.lb_topic)
+        self.rb_subscriber = self.get_xbox_subscriber(self.rb_topic)
+        self.dpad_lr_subscriber = self.get_xbox_subscriber(self.dpad_lr_topic) 
+        self.pause_subscriber = self.get_xbox_subscriber(self.pause_topic)
 
         self.publishers_dict = {
                 'motor_command_exposed/left_trigger': self.create_publisher(Float32, 'motor_control/left_trigger', 10),
@@ -64,41 +50,39 @@ class CommandReciever(Node):
                 'motor_command_exposed/pause': self.create_publisher(Float32, 'motor_control/pause', 10)
                 }
 
+    def parameters_callback(self, params):
+        self.manual_control = self.get_parameter('manual_control').value
+        return SetParametersResult(successful=True)
+        
 
     def get_xbox_subscriber(self, topic):
+        topic_callback_lambda = lambda x: self.callback(x, topic)
         return self.create_subscription(
                     Float32,
                     topic,
-                    self.callback_dict[topic],
+                    topic_callback_lambda,
                     10
                     )
 
-    def lt_callback(self, msg):
-        self.callback(msg, self.lt_topic)
-
-    def rt_callback(self, msg):
-        self.callback(msg, self.rt_topic)
-
-    def lb_callback(self, msg):
-        self.callback(msg, self.lb_topic)
-
-    def rb_callback(self, msg):
-        self.callback(msg, self.rb_topic)
-
-    def dpad_lr_callback(self, msg):
-        self.callback(msg, self.dpad_lr_topic)
-
-    def pause_callback(self, msg):
-        self.callback(msg, self.pause_topic)
-
     def callback(self, msg, topic):
+        self.get_logger().info(topic)
+        self.get_logger().info(type(msg.data))
+        if topic == 'motor_command_exposed/pause' and msg.data == 1.:
+            self.get_logger().info("toggle pause?")
+            manual_ctrl_current = self.get_parameter('manual_control').value
+            man_ctrl = Parameter('manual_control', Parameter.Type.BOOLEAN, not manual_ctrl_current)
+            self.set_parameters([man_ctrl])
+
         if self.manual_control:
             if topic is not None:
                 self.publishers_dict[topic].publish(msg)
                 self.get_logger().info("Publishing:'%s'" % msg.data)
+                self.get_logger().info("manual control state: '%s'" % self.manual_control)
             else:
                 self.get_logger().info(str("None"))
                 self.get_logger().info(str(msg.data))
+        # else:
+        #     self.get_logger().info(str("You are not in manual control mode"))
 
         # self.get_logger().info('I heard: "%s"' % msg.data)
 
